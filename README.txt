f3b8bdeb144e7a1b45a31ca0340a186b5f3fdfe5
mailjean <mailjean00@gmail.com>
2025-06-08 07:27:22 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,11 @@
+
+# Netlift Express Demo Setup
+
+1. Go to https://console.firebase.google.com/
+2. Create a new project (e.g., netlift-express)
+3. Add Firestore Database and enable authentication (email/password)
+4. Get your Firebase config and replace it in firebase.js
+5. Upload the site to https://app.netlify.com/ or Replit to get a free domain
+6. Use 'admin.html' to log in and manage parcels
+
+Enjoy!
---
+++ firebase.js
@@ -0,0 +1,13 @@
+// firebase.js - compatible with Firebase v8.x
+var firebaseConfig = {
+  apiKey: "AIzaSyDuNEvMGx5mKpoB1WAtTa5uhj-6gR8xEdM",
+  authDomain: "netlift-express.firebaseapp.com",
+  projectId: "netlift-express",
+  storageBucket: "netlift-express.appspot.com",  // fixed typo here
+  messagingSenderId: "539332832953",
+  appId: "1:539332832953:web:758960620445a7ff623568",
+  measurementId: "G-FC5WSLNBT3"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
